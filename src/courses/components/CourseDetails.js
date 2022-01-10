b070e80f5acc4cce5c0b390819d416e2a5c0e1de
David Reusch <dbreusch@me.com>
2022-01-10 10:21:11 -0800

    Refactoring CourseDetails to get course metadata from coursesdb-api

--- src/courses/components/CourseDetails.js
+++ src/courses/components/CourseDetails.js
@@ -1,83 +1,109 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import { CourseMetadata } from '../formData/CourseMetadata';
 import Card from '../../shared/components/UIElements/Card';
 import './CourseDetails.css';
 
-const formData = new CourseMetadata();
+// const formData = new CourseMetadata();
 // const formMetaData = formData.formMetaData;
 
 const CourseDetails = props => {
   const auth = useContext(AuthContext);
 
-  const renderItems = formData.fullMetadata.map((field, index) => {
-    let fieldId;
-    if (field.alias) {
-      fieldId = field.alias;
-    } else {
-      fieldId = field.id;
-    }
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-    let fieldLabel = field.label;
-    let fieldValue = props.course[fieldId];
-    let fieldValueClass = null;
-    if (auth.isAdmin || field.isPublic || auth.userId === props.course['creator']) {
-      if (field.type === "date") {
-        fieldValue = fieldValue.split('T', 1);
-        console.log(fieldValue);
-        if (fieldValue[0] === "1970-01-01") {
+  const [fullMetadata, setFullMetadata] = useState();
+  const [formData,] = useState(new CourseMetadata());
+  // eslint-disable-next-line
+  const [redo, setRedo] = useState(false);
+
+  const renderItems = useRef([]);
+
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'https://localhost';
+  const backendPort = process.env.REACT_APP_BACKEND_PORT_C || 3002;
+
+  // get course metadata from backend
+  useEffect(() => {
+    const fetchMetadata = async () => {
+      try {
+        // console.log('Fetching metadata...');
+        const responseData = await sendRequest(`${backendUrl}:${backendPort}/getMetadata`);
+        setFullMetadata(responseData.metadata);
+      } catch (err) {
+        console.log('Error fetching metadata');
+        console.log(err.message);
+      }
+    };
+    fetchMetadata();
+  }, [sendRequest, backendPort, backendUrl]);
+
+  // update formData object when metadata changes
+  useEffect(() => {
+    if (fullMetadata) {
+      // console.log('Updating full metadata...');
+      formData.fullMetadata = fullMetadata;
+      setRedo(true);
+    }
+    // eslint-disable-next-line
+  }, [fullMetadata]);
+
+  // update renderItems when metadata changes
+  useEffect(() => {
+    if (formData.fullMetadata && formData.fullMetadata.length > 0) {
+      renderItems.current = formData.fullMetadata.map((field, index) => {
+        let fieldId;
+        if (field.alias) {
+          fieldId = field.alias;
+        } else {
+          fieldId = field.id;
+        }
+
+        let fieldLabel = field.label;
+        let fieldValue = props.course[fieldId];
+        let fieldValueClass = null;
+        if (auth.isAdmin || field.isPublic || auth.userId === props.course['creator']) {
+          if (field.type === "date") {
+            fieldValue = fieldValue.split('T', 1);
+            if (fieldValue[0] === "1970-01-01") {
+              fieldValue = null;
+            }
+          }
+        } else {
+          fieldLabel = null;
           fieldValue = null;
         }
-      }
-    } else {
-      fieldLabel = null;
-      fieldValue = null;
-    }
 
-    if (fieldLabel && field.isScrollable) {
-      fieldValueClass = 'course-details--scrollbar';
-    }
-    if (fieldLabel && !fieldValue) {
-      fieldValue = 'Empty';
-      fieldValueClass = 'course-details--italic';
-    }
-
-    return <React.Fragment key={index}>
-      <div className="course-details--container" key={index}>
-        <p className="course-details--label">{fieldLabel} </p>
-        <p className={`course-details--value ${fieldValueClass}`}>{fieldValue} </p>
-        {/* {field.id === "n" && <p className="course-details--value">{props.course["purchaseSequence"]} </p>}
-      {field.id !== "n" && <p className="course-details--value">{props.course[field.id]} </p>} */}
-      </div>
-    </React.Fragment>;
-  }
-  );
-
-  return (
-
-    <Card className="course-details">
-
-      {/* {auth.userId === props.course.creator && (
-          <h1>
-            All creator details available!
-          </h1>
-        )
+        if (fieldLabel && field.isScrollable) {
+          fieldValueClass = 'course-details--scrollbar';
+        }
+        if (fieldLabel && !fieldValue) {
+          fieldValue = 'Empty';
+          fieldValueClass = 'course-details--italic';
         }
 
-        {auth.isAdmin && (
-          <h1>
-            All admin details available!
-          </h1>
-        )
-        } */}
-      {/*
-        {formMetaData.map(field => {
-          <p> {field.id} </p>;
-        })} */}
+        return <React.Fragment key={index}>
+          <div className="course-details--container" key={index}>
+            <p className="course-details--label">{fieldLabel} </p>
+            <p className={`course-details--value ${fieldValueClass}`}>{fieldValue} </p>
+          </div>
+        </React.Fragment>;
+      }
+      );
+      setRedo(false);
+    }
+  }, [formData.fullMetadata, auth.isAdmin, auth.userId, props.course]);
 
-      {renderItems}
-
-    </Card>
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && <LoadingSpinner asOverlay />}
+      <Card className="course-details">
+        {renderItems.current}
+      </Card>
+    </React.Fragment>
   );
 };
 
